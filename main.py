5955a88e2052230af33d0c0c0bb0ac6f9596a69c
borks <steveminecraft899@gmail.com>
2024-06-05 23:21:54 -0500

    first branch commit

--- main.py
+++ main.py
@@ -1,39 +1,63 @@
-# example painter project
+import pygame
 
-from painter import Painter
+pygame.init()
 
-p = Painter(10, 2, 1, "east", 100, 2)
+screen_height = 1920
+screen_width = 1080
+#! useful flags: pygame.FULLSCREEN, pygame.RESIZABLE, pygame.NOFRAME, pygame.SCALED, pygame.SHOWN, pygame.HIDDEN
+screen = pygame.display.set_mode((screen_height, screen_width))
+clock = pygame.time.Clock()
+dt = 0
+running = True
 
-p.move()
-p.turnLeft()
-p.move()
-p.paint("blue")
-p.turnLeft()
-p.move()
-p.paint("#44a67a")
-p.turnLeft()
-p.turnLeft()
-p.move()
-p.paint("red")
-print(p.isOnPaint())
-p.turnLeft()
-p.turnLeft()
-p.move()
-p.scrapePaint()
-p.turnLeft()
-p.paint("green")
-p.faceSouth()
-while p.canMove():
-    c = p.getColor()
-    p.move()
-    if c == "green":
-        p.paint("orange")
-    else:
-        p.paint("green")
-p.turnLeft()
-p.move()
-p.paint("#FF7777")
-print(p.isOnPaint())
-p.turnLeft()
-p.turnLeft()
-p.move()
+#! other useful things
+#! pygame.display.get_active()
+#! pygame.display.get_desktop_sizes() -> find suitable sizes for non-fullscreen
+#! pygame.display.list_modes() -> find suitable sizes for fullscreen
+#! pygame.display.set_caption("my name jeff")
+#! pygame.display.iconify()
+#! pygame.display.toggle_fullscreen()
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        #! other events
+        #! KEYDOWN (key, mod, unicode, scancode)
+        #! KEYUP (~)
+        #! MOUSEMOTION (pos, rel, buttons, touch)
+        #! MOUSEBUTTONUP (pos, button, touch)
+        #! MOUSEBUTTONDOWN (~)
+
+        #! window events
+        #! ALL HAVE PREFIX "WINDOW"
+        #! SHOWN, HIDDEN, EXPOSED, MOVED, RESIZED, SIZECHANGED, MINIMIZED, MAXIMIZED,
+        #! RESTORED, ENTER, LEAVE, FOCUSGAINED/LOST, CLOSE, DISPLAYCHANGED
+
+        #! sample code for text input
+    #     if event.type == pygame.KEYDOWN:
+    #         # check backspace
+    #         if event.key == pygame.K_BACKSPACE and blanks_text:
+    #             blanks_text = blanks_text[:-1]
+    #             backspace = True
+    #             backspace_start = time.time()
+    #         if event.key == pygame.K_RETURN and blanks_text:
+    #             blanks_storage.append(blanks_text)
+    #             blanks_text = ""
+    #             i += 1
+    #     if event.type == pygame.KEYUP:
+    #         if event.key == pygame.K_BACKSPACE:
+    #             backspace = False
+    #             backspace_start = None
+    #     if event.type == pygame.TEXTINPUT:
+    #         blanks_text += event.text
+    #
+    # if backspace and time.time() - backspace_start > 0.5:
+    #     if backspace_rapid and time.time() - backspace_rapid > 0.05:
+    #         blanks_text = blanks_text[:-1]
+    #         backspace_rapid = time.time()
+    #     elif not backspace_rapid:
+    #         backspace_rapid = time.time()
+
+    #! rest of loop code here
